7e55c3fe513a4566a3af76a7afd7128ec77d0fa4
Juan Diego Chaparro <juan.chaparro@aurotech.co>
2024-07-21 15:46:59 -0500

    my first commit

---
+++ EnfermeriaProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnfermeriaProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
